47a95d8f9c28778a0ca487da630047048d5843a9
Biruk Tassew <biruktassew21@gmail.com>
2023-04-11 16:24:56 +0300

    feat(backend): add database context for the models

---
+++ Migrations/20230411131419_init_migration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace restAPI.Migrations
+{
+    public partial class init_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "user_schema");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:gender", "MALE,FEMALE");
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                schema: "user_schema",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    age = table.Column<int>(type: "integer", nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user",
+                schema: "user_schema");
+        }
+    }
+}
