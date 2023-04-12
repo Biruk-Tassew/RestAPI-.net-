8139e0e1039a13059d79ba9b45285347e2d0444b
Biruk Tassew <biruktassew21@gmail.com>
2023-04-12 11:35:54 +0300

    feat(backend): Add CRUD capability for Team

--- Program.cs
+++ Program.cs
@@ -1,21 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using restAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApiDbContext>(optionsAction: options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString(name: "ConnectionString"))
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
--- controllers/TeamsController.cs
+++ controllers/TeamsController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restAPI.Data;
+using restAPI.Models;
 
 namespace restAPI.Controllers;
 
@@ -6,10 +9,63 @@ namespace restAPI.Controllers;
 [ApiController]
 public class TeamsController : ControllerBase
 {
-    [HttpGet]
-    public IActionResult Get()
+    private static ApiDbContext _contex;
+    public TeamsController (ApiDbContext context)
     {
-        return Ok("Ok from the controller");
+        _contex = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var teams = await _contex.Teams.ToListAsync();
+        return Ok(teams);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get (int id){
+        var team = await _contex.Teams.FirstOrDefaultAsync(t => t.id == id);
+
+        if (team == null)
+            return BadRequest(error: "Invalid Id");
+
+        return Ok(team);
+
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post (Team team) {
+        await _contex.AddAsync(team);
+        await _contex.SaveChangesAsync();
+
+        return CreatedAtAction("Get", team.id, team);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> Patch (int id, string name)
+    {
+        var team = await _contex.Teams.FirstOrDefaultAsync(t => t.id == id);
+
+        if (team == null)
+            return BadRequest(error: "Invalid id");
+
+        team.Name = name;
+        await _contex.SaveChangesAsync();
+        return Ok(team);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete (int id)
+    {
+        var team = await _contex.Teams.FirstOrDefaultAsync(t => t.id == id);
+
+        if (team == null)
+            return BadRequest(error: "Invalid id");
+
+        _contex.Remove(team);
+        await _contex.SaveChangesAsync();
+
+        return NoContent();
     }
 
 }
